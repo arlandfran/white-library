5792a9672d808c555ac160ba407498d59dfaa86c
Arland <arland.torres@outlook.com>
2022-02-07 22:52:49 +0000

    add product sorting

--- static/js/index.js
+++ static/js/index.js
@@ -24,3 +24,23 @@ function selectCategory() {
     window.location.replace(currentUrl);
   }
 }
+
+function sortProducts() {
+  var select = document.getElementById("sort_selector");
+  var currentUrl = new URL(window.location);
+
+  var val = select.value;
+
+  if (val != "reset") {
+    var sort = val.split("_")[0];
+    var direction = val.split("_")[1];
+
+    currentUrl.searchParams.set("sort", sort);
+    currentUrl.searchParams.set("direction", direction);
+    window.location.replace(currentUrl);
+  } else {
+    currentUrl.searchParams.delete("sort");
+    currentUrl.searchParams.delete("direction");
+    window.location.replace(currentUrl);
+  }
+}
