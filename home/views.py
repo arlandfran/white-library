62c27d424cdcf82e83d0594dea24dc7fe26f12e3
Arland <arland.torres@outlook.com>
2022-02-08 01:02:01 +0000

    add home page including latest products
    - closes #9

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def index(request):
-  """Return Index page"""
-  return render(request, 'home/index.html')
+  """Return Home page, including latest products"""
+
+  latest_products = Product.objects.order_by('-id')[:6]
+
+  context = {
+    'products': latest_products
+  }
+
+  return render(request, 'home/index.html', context)
