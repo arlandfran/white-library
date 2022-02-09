3e210e1a9b1cbb86eb6d47caf3335eeb1c2baca6
Arland <arland.torres@outlook.com>
2022-02-09 01:46:05 +0000

    update bag item context and show bag count in UI

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,23 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 def bag_contents(request):
 
   bag_items = []
-  product_count = 0
+  total = 0
+  bag = request.session.get('bag', {})
+
+  for item_id, quantity in bag.items():
+    product = get_object_or_404(Product, pk=item_id)
+    total += quantity * product.price
+    bag_items.append({
+      'item_id': item_id,
+      'quantity': quantity,
+      'product': product,
+    })
 
   context = {
     'bag_items': bag_items,
-    'product_count': product_count,
   }
 
   return context
