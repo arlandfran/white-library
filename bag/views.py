b86cf4dba0eae20d709492cb59d7ae5ed648d064
Arland <arland.torres@outlook.com>
2022-02-09 00:49:33 +0000

    implement add to bag
    - close #14

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_bag(request):
   """Returns shopping bag"""
 
   return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+  """Add product to bag"""
+
+  redirect_url = request.POST.get('redirect_url')
+  bag = request.session.get('bag', {})
+
+  if item_id not in list(bag.keys()):
+    bag[item_id] = 1
+  
+  request.session['bag'] = bag
+  
+  return redirect(redirect_url)
