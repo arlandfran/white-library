2d87212edef94c76ad51c92aaaa1e64574986514
Arland <arland.torres@outlook.com>
2022-02-10 01:08:38 +0000

    add messages for bag actions

--- bag/views.py
+++ bag/views.py
@@ -11,12 +11,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
   """Add product to bag"""
 
+  product= get_object_or_404(Product, pk=item_id)
   redirect_url = request.POST.get('redirect_url')
   bag = request.session.get('bag', {})
 
   if item_id not in list(bag.keys()):
     bag[item_id] = 1
-  
+
+  messages.success(request, f'{product.name} has been added to your bag')
   request.session['bag'] = bag
   
   return redirect(redirect_url)
@@ -28,7 +30,7 @@ def remove_from_bag(request, item_id):
     product= get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
     bag.pop(item_id)
-    messages.success(request, f'Removed {product.name} from your bag')
+    messages.success(request, f'{product.name} has been removed from your bag')
     request.session['bag'] = bag
     return HttpResponse(status=200)
   except Exception as e:
