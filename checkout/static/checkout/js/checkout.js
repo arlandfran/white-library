0f8d5d29efe8358e63a1859cd66dee53f5383704
arland <arland.torres@outlook.com>
2022-02-20 23:44:05 +0000

    add profile menu llnks
    - change account references to profile
    - other small css changes

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -12,6 +12,7 @@ const options = {
       fontFamily:
         'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       borderRadius: "0",
+      colorBackground: "white",
     },
     rules: {
       ".Label": {
