fc85d027730bd454041935cd099a2ce5cd19a92c
arland <arland.torres@outlook.com>
2022-02-23 23:17:14 +0000

    implement add to saved products

--- products/views.py
+++ products/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import Product, Category
+
+from profiles.models import UserProfile, SavedProduct
 
 
 def all_products(request):
@@ -66,3 +70,22 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def save(request, product_id):
+    """Save product to user profile"""
+
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+    redirect_url = request.POST.get('redirect_url')
+
+    try:
+        saved = user_profile.saved.get(product=product)
+        messages.error(request, f'{saved.product.name} already saved')
+    except ObjectDoesNotExist:
+        saved_product = SavedProduct(
+            profile=user_profile, product=product)
+        saved_product.save()
+        messages.success(request, 'Product saved successfully')
+
+    return redirect(redirect_url)
